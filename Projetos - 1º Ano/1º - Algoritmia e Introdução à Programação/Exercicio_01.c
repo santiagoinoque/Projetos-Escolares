6c15df3d325ba67e986246a3142a8f6d8b730a3e
Santiago Inoque <santiagoinoque09@gmail.com>
2026-05-06 20:30:47 +0100

    Update Exercicio_01.c
    add code to the exercice 1

--- Projetos - 1º Ano/1º - Algoritmia e Introdução à Programação/Exercicio_01.c
+++ Projetos - 1º Ano/1º - Algoritmia e Introdução à Programação/Exercicio_01.c
@@ -1,1 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main() 
+{
+	int kbytes, veloc;
+
+	printf("Indique o tamanho (KBytes): ");
+	scanf("%d", &kbytes);
+	printf("Indique a velocidade de transmissao (B/s): ");
+	scanf("%d", &veloc);
+	printf("Tempo de upload (s): %.6lf \n\n", (double)kbytes * 1024 / veloc);
+
+	return 0;
+}
