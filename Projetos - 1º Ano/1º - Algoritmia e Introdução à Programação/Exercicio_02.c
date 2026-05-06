c271a7fb69642bcc4ef4a190870d1d2a9670629e
Santiago Inoque <santiagoinoque09@gmail.com>
2026-05-07 00:57:02 +0100

    Update Exercicio_02.c

--- Projetos - 1º Ano/1º - Algoritmia e Introdução à Programação/Exercicio_02.c
+++ Projetos - 1º Ano/1º - Algoritmia e Introdução à Programação/Exercicio_02.c
@@ -1,1 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main() 
+{
+	double cerca, raio;
+
+	printf("Comprimento da cerca: ");
+	scanf("%lf", &cerca);
+
+	cerca *= 100.0;
+
+	raio = cerca / 3.14;
+
+	printf("Valor do raio: %.2lf\n\n", raio);
+
+	return 0;
+}
