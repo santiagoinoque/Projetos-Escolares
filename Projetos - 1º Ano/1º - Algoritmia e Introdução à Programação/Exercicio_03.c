7e253a133c4613ab1f7f5973f0a34e45a47444d0
Santiago Inoque <santiagoinoque09@gmail.com>
2026-05-08 20:16:30 +0100

    Create Exercicio_03.c

---
+++ Projetos - 1º Ano/1º - Algoritmia e Introdução à Programação/Exercicio_03.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double altura, diametro, raio, volume_litros, dias, pi = M_PI;
+
+    printf("Indique a altura do reservatorio: ");
+    scanf("%lf", &altura);
+
+    printf("Indique o diametro da base: ");
+    scanf("%lf", &diametro);
+
+    raio = diametro / 2;
+    volume_litros = pi * (raio * raio) * altura / 1000;
+    dias = volume_litros / (185 * 3);
+
+    printf("Dias de abastecimento: %.2f\n", dias);
+
+    return 0;
+}
