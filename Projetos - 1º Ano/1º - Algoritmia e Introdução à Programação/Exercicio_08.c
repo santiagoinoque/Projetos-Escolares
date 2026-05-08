0a50c43830f5fb85dc0696f23cadca0bcc31fe57
Santiago Inoque <santiagoinoque09@gmail.com>
2026-05-08 20:35:16 +0100

    Create Exercicio_08.c

---
+++ Projetos - 1º Ano/1º - Algoritmia e Introdução à Programação/Exercicio_08.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int a, b, maior, menor;
+
+    printf("Indique os valores: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b) 
+    { 
+        maior = a; 
+        menor = b; 
+    }
+    else 
+    { 
+        maior = b; 
+        menor = a; 
+    }
+
+    printf("Maior valor: %d\n", maior);
+    printf("Div. Real: %.1lf\n", (double)maior / menor);
+
+    return 0;
+}
